45d9cd7c82ad721eba94e5df115ff51b34b82793
otavioobraga <otavioobraga@gmail.com>
2023-11-28 10:30:35 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosProdutoSharp {
+    internal class Produto {
+        public string Nome;
+        public float Preco;
+        public int Quantidade;
+
+        public double TotalEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+            string formatoPreco = Preco.ToString("F2");
+            string formatoTotalEstoque = TotalEstoque().ToString("F2");
+
+            string descricaoProduto = $"{Nome}, ${formatoPreco}";
+            string informacoesEstoque = $"{Quantidade} unidades, Total: $ {formatoTotalEstoque}";
+
+            return $"{descricaoProduto}, {informacoesEstoque}"; 
+        }
+
+        public int AddProduct(int quantity) {
+            Quantidade += quantity;
+            return Quantidade;
+        }
+        public int DeleteProduct(int quantity) {
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using DadosProdutoSharp;
+using System;
+
+namespace ProductSharp {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto(); 
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = float.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
